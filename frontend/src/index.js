926055b66edf13d4c03c16882c214c8f8738f3be
finani <finani007@gmail.com>
2025-11-07 20:14:29 -0500

    Configured Auth0 Provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="https://api.medinfo-hackathon.com"
+    clientId="MYyGr6UKBvMH5t3ZiQBh1Fu8IP7FnyYp"
+    authorizationParams={{
+        redirect_uri: window.location.origin + '/dashboard',
+        audience: "https://api.medinfo-hackathon.com"
+     }}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
