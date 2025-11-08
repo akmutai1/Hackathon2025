89e65d803800dbf1e942d9d8f5bc9e3dae431797
finani <finani007@gmail.com>
2025-11-08 11:06:10 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
+// /frontend/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = "dev-fufeub15o8ylzslm.us.auth0.com";
-const clientId = "MYyGr6UKBvMH5t3ZiQBh1Fu8IP7FnyYp";
-const audience = "https://api.medinfo-hackathon.com";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{ redirect_uri: window.location.origin, audience }}
-  >
-    <App />
-  </Auth0Provider>
+  <React.StrictMode>
+    <Auth0Provider
+      domain="YOUR_AUTH0_DOMAIN" // Paste Domain from Kelvin
+      clientId="YOUR_AUTH0_CLIENT_ID" // Paste Client ID from Kelvin
+      authorizationParams={{
+        redirect_uri: window.location.origin + '/dashboard',
+        audience: "https://api.medinfo-hackathon.com" // Paste API Identifier (Audience) from Kelvin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
 );
